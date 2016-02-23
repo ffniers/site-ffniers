3a95703aa55cb5be723a6230587d477e59e749bf
Thomas Fragstein <thomas@fragstein.de>
2016-02-23 19:01:13 +0100

    Update site.mk

--- site.mk
+++ site.mk
@@ -1,34 +1,39 @@
 GLUON_SITE_PACKAGES := \
         gluon-mesh-batman-adv-14 \
-        gluon-alfred \
-        gluon-announced \
-        gluon-autoupdater \
-        gluon-setup-mode \
-        gluon-config-mode-core \
-        gluon-config-mode-autoupdater \
-        gluon-config-mode-hostname \
-        gluon-config-mode-mesh-vpn \
-        gluon-config-mode-geo-location \
-        gluon-config-mode-contact-info \
-        gluon-ebtables-filter-multicast \
-        gluon-ebtables-filter-ra-dhcp \
-        gluon-luci-admin \
-        gluon-luci-autoupdater \
-        gluon-luci-mesh-vpn-fastd \
+	gluon-alfred \
+	gluon-respondd \
+	gluon-autoupdater \
+	gluon-neighbour-info \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-contact-info \
+	gluon-config-mode-core \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-hostname \
+	gluon-config-mode-tunneldigger \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-luci-theme \
+	gluon-luci-admin \
+	gluon-luci-autoupdater \
         gluon-luci-portconfig \
-        gluon-luci-private-wifi \
-        gluon-luci-wifi-config \
-        gluon-next-node \
-        gluon-mesh-vpn-fastd \
-        gluon-radvd \
-        gluon-status-page \
-        iwinfo \
-        iptables \
-        haveged
+	gluon-luci-wifi-config \
+	gluon-luci-private-wifi \
+	gluon-next-node \
+	gluon-mesh-vpn-tunneldigger \
+	gluon-radvd \
+	gluon-setup-mode \
+	gluon-ssid-changer \
+	gluon-status-page \
+	gluon-migrate-vpn \
+	gluon-tunneldigger-watchdog \
+	haveged \
+	iptables \
+	iwinfo
 
-GLUON_LANGS = en de
+GLUON_LANGS ?= en de
 
-DEFAULT_GLUON_RELEASE := 0.7.4
+#DEFAULT_GLUON_RELEASE := 0.7-$(GLUON_BRANCH)-$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.8.0
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
