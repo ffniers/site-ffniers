2f92e434922b04c7d6e615b3301d2d4fa1c7e708
Thomas Fragstein <thomas@fragstein.de>
2017-06-12 21:51:58 +0200

    version 0.8.3

--- site.mk
+++ site.mk
@@ -36,7 +36,7 @@ GLUON_SITE_PACKAGES := \
 GLUON_LANGS ?= en de
 
 #DEFAULT_GLUON_RELEASE := 0.7-$(GLUON_BRANCH)-$(shell date '+%Y%m%d')
-DEFAULT_GLUON_RELEASE := 0.8.2tf
+DEFAULT_GLUON_RELEASE := 0.8.3
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
