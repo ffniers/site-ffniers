d128caf3928c77042afee9b60f3ffe22da2febc9
vincent-ff-du <36100246+vincent-ff-du@users.noreply.github.com>
2018-04-07 08:54:17 +0200

    Add files via upload
    Config für LEDE 2017.1.5 Niersufer Instanz

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # site-ffniers
 FF Niersufer (FFNIERS) specific Gluon configuration.
+LEDE 2017.1.5 Beta rev.0.9.0
--- site.mk
+++ site.mk
@@ -1,7 +1,8 @@
 GLUON_SITE_PACKAGES := \
-        gluon-mesh-batman-adv-14 \
+	gluon-mesh-batman-adv-14 \
 	gluon-alfred \
-	gluon-announced \
+	gluon-respondd \
+	gluon-autorestart \
 	gluon-autoupdater \
 	gluon-config-mode-autoupdater \
 	gluon-config-mode-contact-info \
@@ -11,29 +12,74 @@ GLUON_SITE_PACKAGES := \
 	gluon-config-mode-mesh-vpn \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
-	gluon-luci-admin \
-	gluon-luci-autoupdater \
-        gluon-luci-portconfig \
-	gluon-luci-wifi-config \
-	gluon-luci-theme \
-	gluon-luci-private-wifi \
-        gluon-luci-mesh-vpn-fastd \
-	gluon-next-node \
-	gluon-mesh-vpn-fastd \
+	gluon-ebtables-source-filter \
+	gluon-web-admin \
+	gluon-web-autoupdater \
+	gluon-web-wifi-config \
+	gluon-web-private-wifi \
+	gluon-web-network \
+	gluon-mesh-vpn-tunneldigger \
+	gluon-tunneldigger-watchdog \
 	gluon-radvd \
 	gluon-setup-mode \
-	gluon-status-page \
-	haveged \
-	iptables \
-	iwinfo
+	ffac-status-page \
+	gluon-ssid-changer \
+	iwinfo \
+	haveged
 
-GLUON_LANGS ?= en de
+USB_BASIC := \
+	kmod-usb-core \
+	kmod-usb2 \
+	kmod-usb-hid
 
-DEFAULT_GLUON_RELEASE := 0.8.3
+USB_NIC := \
+	kmod-usb-net \
+	kmod-usb-net-asix \
+	kmod-usb-net-rtl8150 \
+	kmod-usb-net-rtl8152 \
+	kmod-usb-net-dm9601-ether
+
+USB_WIFI := \
+	kmod-rtl8192cu
+
+ifeq ($(GLUON_TARGET),x86-generic)
+	GLUON_SITE_PACKAGES += \
+		$(USB_BASIC) \
+		kmod-usb-ohci-pci \
+		$(USB_NIC)
+endif
+
+ifeq ($(GLUON_TARGET),x86-64)
+	GLUON_SITE_PACKAGES += \
+		$(USB_BASIC) \
+		$(USB_NIC) \
+		kmod-igb #APU2
+endif
+
+ifeq ($(GLUON_TARGET),brcm2708-bcm2708)
+	GLUON_SITE_PACKAGES += \
+		$(USB_BASIC) \
+		$(USB_NIC) \
+		$(USB_WIFI)
+endif
+
+ifeq ($(GLUON_TARGET),brcm2708-bcm2709)
+	GLUON_SITE_PACKAGES += \
+		$(USB_BASIC) \
+		$(USB_NIC) \
+		$(USB_WIFI)
+endif
+
+DEFAULT_GLUON_RELEASE := 0.9.01
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
+# Default priority for updates.
 GLUON_PRIORITY ?= 0
 
-GLUON_REGION= eu 
+GLUON_LANGS ?= en de
+
+GLUON_ATH10K_MESH ?= 11s
+
+GLUON_REGION := eu
